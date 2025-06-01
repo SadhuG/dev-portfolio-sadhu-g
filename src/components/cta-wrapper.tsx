e5dc2bfcf0eb446b4de868c937f7b870548380d5
Sudhansh <96883913+SadhuG@users.noreply.github.com>
2025-06-01 16:34:48 +0530

    added mobile font and additional font classes, responsive hero and CTA wrapper

--- src/components/cta-wrapper.tsx
+++ src/components/cta-wrapper.tsx
@@ -4,26 +4,30 @@ import { Button } from "./ui/button";
 
 const CtaWrapper = () => {
 	return (
-		<div className="flex gap-6">
-			<Button
-				variant={"outline"}
-				className="gap-3 group relative overflow-hidden"
-			>
-				<span className="relative z-10 transition-colors duration-500 ease-in-out group-hover:text-background">
-					Contact me
-				</span>
-				<div className="relative z-10 size-4 rounded-full bg-foreground">
-					<ArrowUpRight className="text-background" />
-				</div>
-				<div className="absolute right-6 top-1/2 -translate-y-1/2 size-4 rounded-full bg-foreground transition-transform duration-500 ease-in-out group-hover:scale-[15]"></div>
-			</Button>
-			<Button
-				variant={"tertiary"}
-				size={"tertiary"}
-			>
-				<Copy />
-				hello@sadhug.in
-			</Button>
+		<div className="flex flex-col gap-6">
+			<div>
+				<Button
+					variant={"outline"}
+					className="gap-3 group relative overflow-hidden"
+				>
+					<span className="relative z-10 transition-colors duration-500 ease-in-out group-hover:text-background">
+						Contact me
+					</span>
+					<div className="relative z-10 size-4 rounded-full bg-foreground">
+						<ArrowUpRight className="text-background" />
+					</div>
+					<div className="absolute right-6 top-1/2 -translate-y-1/2 size-4 rounded-full bg-foreground transition-transform duration-500 ease-in-out group-hover:scale-[15]"></div>
+				</Button>
+			</div>
+			<div>
+				<Button
+					variant={"tertiary"}
+					size={"tertiary"}
+				>
+					<Copy />
+					hello@sadhug.in
+				</Button>
+			</div>
 		</div>
 	);
 };
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,12 +5,12 @@ const Hero = () => {
 	return (
 		<section className="h-full flex items-center">
 			<div>
-				<div className="w-[65%] mb-8">
+				<div className="w-full max-w-3xl mb-8">
 					<h1>
 						I create websites that work with <br />
 						<span className="font-accent">Design that Speaks</span>
 					</h1>
-					<p className="text-[26px] leading-[160%] mt-6">
+					<p className="text-large-normal mt-6">
 						<span className="mr-6">Hello! I’m Sudhansh</span>
 						<span>a frontend dev.</span>
 					</p>
