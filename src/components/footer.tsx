29f46dae35dde582e68be48aa434efdecf5298bd
Sudhansh <96883913+SadhuG@users.noreply.github.com>
2025-06-02 22:32:17 +0530

    responsive footer completed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,58 +7,49 @@ import SocialLinks from "./social-links";
 import { Button } from "./ui/button";
 
 const Footer = () => {
-	const footerMenu = [
-		{ text: "Mission", href: "#" },
-		{ text: "Works", href: "#" },
-		{ text: "About", href: "#" },
-	];
+  const footerMenu = [
+    { text: "Mission", href: "#" },
+    { text: "Works", href: "#" },
+    { text: "About", href: "#" },
+  ];
 
-	return (
-		<footer className="px-5 py-12 sm:p-16 lg:px-16 lg:py-20">
-			<div className="flex justify-between">
-				<div>
-					<span className="text-lg">© Sudhansh Singh 2025.</span>
-				</div>
-				<div className="flex flex-col ">
-					{footerMenu.map((item, index) => (
-						<Link
-							key={index}
-							href={item.href}
-							className="font-semibold text-base/[160%]"
-						>
-							{item.text}
-						</Link>
-					))}
-				</div>
-				<div>
-					<div>
-						<Button
-							size={"tertiary"}
-							variant={"tertiary"}
-						>
-							<Image
-								src={mailSend}
-								alt={"mail send icon"}
-							/>
-							hello@sadhug.in
-						</Button>
-					</div>
-					<div className="w-full flex justify-end">
-						<SocialLinks />
-					</div>
-				</div>
-				<div>
-					<Button
-						variant={"outlineSecondary"}
-						size={"sm"}
-					>
-						Back to top
-						<CircleArrowUp />
-					</Button>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="px-5 py-12 sm:p-16 lg:px-16 lg:py-20">
+      <div className="flex flex-col items-center gap-8 lg:flex-row lg:items-start lg:justify-between">
+        <div className="order-last lg:order-none">
+          <span className="text-lg">© Sudhansh Singh 2025.</span>
+        </div>
+        <div className="flex flex-col items-center lg:w-28 lg:items-end xl:w-56">
+          {footerMenu.map((item, index) => (
+            <Link
+              key={index}
+              href={item.href}
+              className="text-base/[160%] font-semibold"
+            >
+              {item.text}
+            </Link>
+          ))}
+        </div>
+        <div className="space-y-2">
+          <div>
+            <Button size={"tertiary"} variant={"tertiary"}>
+              <Image src={mailSend} alt={"mail send icon"} />
+              hello@sadhug.in
+            </Button>
+          </div>
+          <div className="flex w-full justify-center lg:justify-end">
+            <SocialLinks />
+          </div>
+        </div>
+        <div>
+          <Button variant={"outlineSecondary"} size={"sm"}>
+            Back to top
+            <CircleArrowUp />
+          </Button>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
